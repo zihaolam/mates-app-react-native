7516f0d38374266e6414d4f2991bd49c8f7e4f77
LAPTOP-R3SEKBDG\Tamara <tamara6@connect.hku.hk>
2021-11-17 21:43:30 +0800

    added notice board and to-do pages

--- src/contexts/provider.js
+++ src/contexts/provider.js
@@ -1,8 +1,14 @@
 import { PaymentProvider } from "./payment";
 import { UserProvider } from "./user";
 import { RoomProvider } from "./room";
-import React from 'react';
+import React from "react";
 
-const ContextProviders = ({children}) => <PaymentProvider><UserProvider><RoomProvider>{children}</RoomProvider></UserProvider></PaymentProvider>
+const ContextProviders = ({ children }) => (
+  <PaymentProvider>
+    <UserProvider>
+      <RoomProvider>{children}</RoomProvider>
+    </UserProvider>
+  </PaymentProvider>
+);
 
 export default ContextProviders;
