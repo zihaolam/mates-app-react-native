4f21fde795ded7fe0090eb4d81857a16d36a22a0
LAPTOP-R3SEKBDG\Tamara <tamara6@connect.hku.hk>
2021-11-21 16:34:21 +0800

    changed homeData order and type

--- src/mocks/homeData.js
+++ src/mocks/homeData.js
@@ -1,41 +1,41 @@
-import { COLORS } from "styles"
+import { COLORS } from "styles";
 
 export const homeData = [
-	{
-		type: 'todo',
-		postedBy: 'zihaolam',
-		text: 'has posted a new todo',
-		createdAt: 1636414902301
-	},
-	{
-		type: 'notice',
-		postedBy: 'tamara',
-		text: 'has posted a new notice',
-		createdAt: 1636414112094
-	},
-	{
-		type: 'payment',
-		postedBy: 'briony',
-		text: 'has posted a new payment',
-		createdAt: 1636414902081
-	},
-	{
-		type: 'food',
-		postedBy: 'inweon',
-		text: 'has updated the grocery list',
-		createdAt: 1636414703000
-	},
-	{
-		type: 'todo',
-		postedBy: 'inweon',
-		text: 'has posted a new todo',
-		createdAt: 1636414703000
-	}
-]
+  {
+    type: "todo",
+    postedBy: "zihaolam",
+    text: "has posted a new todo",
+    createdAt: 1636414902301,
+  },
+  {
+    type: "payment",
+    postedBy: "briony",
+    text: "has posted a new payment",
+    createdAt: 1636414902081,
+  },
+  {
+    type: "todo",
+    postedBy: "tamara",
+    text: "has posted a new notice",
+    createdAt: 1636414112094,
+  },
+  {
+    type: "food",
+    postedBy: "inweon",
+    text: "has updated the grocery list",
+    createdAt: 1636414703000,
+  },
+  {
+    type: "todo",
+    postedBy: "inweon",
+    text: "has posted a new todo",
+    createdAt: 1636414703000,
+  },
+];
 
 export const colorMap = {
-	todo: COLORS.PURPLE,
-	notice: COLORS.BLUE,
-	payment: COLORS.LIGHTPINK,
-	food: COLORS.GREEN
-}
+  todo: COLORS.PURPLE,
+  notice: COLORS.BLUE,
+  payment: COLORS.LIGHTPINK,
+  food: COLORS.GREEN,
+};
